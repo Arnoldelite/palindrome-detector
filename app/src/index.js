427a00259418473775952013ccefca68aaa32d66
Arnoldelite <arnoldelite@yahoo.co.uk>
2019-02-02 00:58:20 -0500

    - dont point to empty route

--- app/src/index.js
+++ app/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'mobx-react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Store from './store'
 import NavBar from './components/NavBar'
-import Popular from './components/Popular'
+// import Popular from './components/Popular'
 import 'antd/dist/antd.css'
 import './style/style.scss'
 
@@ -16,7 +16,7 @@ function Index() {
     <Provider store={store}>
       <Router>
         <div className="index">
-          <Route exact path="/popular" component={Popular} />
+          <Route exact path="/popular" component={NavBar} />
           <Route path="/app" component={NavBar} />
           {/* <Route path="/" component={App} /> */}
         </div>
