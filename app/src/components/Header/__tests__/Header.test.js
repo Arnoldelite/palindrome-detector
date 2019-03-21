2ff73fb5a9b94662111f3810ab4e78078d2978ef
Arnoldelite <arnoldelite@yahoo.co.uk>
2019-03-21 03:08:09 -0400

    - clean test, save prog

--- app/src/components/Header/__tests__/Header.test.js
+++ app/src/components/Header/__tests__/Header.test.js
@@ -16,30 +16,4 @@ describe('<MyComponent />', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  //   it('renders an `.icon-star`', () => {
-  //     const wrapper = shallow(<MyComponent />)
-  //     expect(wrapper.find('.icon-star')).to.have.lengthOf(1)
-  //   })
-
-  //   it('renders children when passed in', () => {
-  //     const wrapper = shallow((
-  //       <MyComponent>
-  //         <div className="unique" />
-  //       </MyComponent>
-  //     ));
-  //     expect(wrapper.contains(<div className="unique" />)).to.equal(true)
-  //   })
-
-  //   describe('fetchMembersIsLoading', () => {
-  //     test('returns true when fetching members', () => {
-  //       const spaceId = 'spaceId3'
-  //       const isLoading = fetchMembersIsLoading(state, spaceId)
-  //       expect(isLoading).toBeTruthy()
-  //     })
-
-  //     test('returns false when page is not found', () => {
-  //       const spaceId = 'not a space id'
-  //       const isLoading = fetchMembersIsLoading(state, spaceId)
-  //       expect(isLoading).toBeFalsy()
-  //     })
 })
