d0518e7dc82e51c82194c41911fcab226eca121b
Arnoldelite <arnoldelite@yahoo.co.uk>
2019-04-12 19:06:28 -0400

    - test

--- app/src/components/Header/__tests__/Header.test.js
+++ app/src/components/Header/__tests__/Header.test.js
@@ -1,17 +1,10 @@
 import React from 'react'
-
 // import Parse from 'parse/node'
 import { shallow } from 'enzyme'
 // import Adapter from 'enzyme-adapter-react-16'
 import Header from '..'
 import { shallowToJson } from 'enzyme-to-json'
 import '../../../../../config/jest/enzyme.config'
-
-// import { expect } from 'chai';
-// import sinon from 'sinon';
-
-// import MyComponent from './MyComponent'
-// import Foo from './Foo'
 
 describe('<Header />', () => {
   beforeEach(() => {
--- app/src/components/MessageInput/__tests__/MessageInput.test.js
+++ app/src/components/MessageInput/__tests__/MessageInput.test.js
@@ -1,25 +1,18 @@
 import React from 'react'
-
 // import Parse from 'parse/node'
-import { shallow, configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { shallow } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16'
 import MessageInput from '..'
 import { shallowToJson } from 'enzyme-to-json'
 import '../../../../../config/jest/enzyme.config'
 
-// import { expect } from 'chai';
-// import sinon from 'sinon';
-
-// import MyComponent from './MyComponent'
-// import Foo from './Foo'
-
 describe('<MessageInput />', () => {
   beforeEach(() => {
-    configure({ adapter: new Adapter() })
+    // configure({ adapter: new Adapter() })
   })
-  test('renders <Header /> component', () => {
+  test('renders <MessageInput /> component', () => {
     const wrapper = shallow(<MessageInput />)
+    // console.log("wrapper", wrapper.instance().state)
     expect(shallowToJson(wrapper)).toMatchSnapshot()
   })
-
 })
